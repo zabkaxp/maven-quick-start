15886413e920c654cdcc71eaa234278bd108628b
Marta <chao@op.pl>
2019-06-08 23:09:33 +0100

    Adding app Java source

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	Application app = new Application();
+    }
+}
